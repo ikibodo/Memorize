20a0328e8909ac0f5b43baf7b6959c22f7b3aac7
kibodo <kibodo@ya.ru>
2024-09-17 20:32:49 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by N L on 17.9.24..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
