ccf4c327bb15557dfd8fe4dca974ec48bcc9e7ad
kibodo <kibodo@ya.ru>
2025-06-10 15:40:54 +0200

    feat: Lecture 1 result

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,25 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "What will happen?"
-    @State private var isToggled = false
-    
-    var body: some View { // вычисляемоей св-во, поведение
-        
-        VStack(alignment: .leading, spacing: 20) { // структура ведущая себя как представление  @ViewBuilder превращает список вьюх в TupleView
-            Image(systemName: "star")
-                .imageScale(.large)
-                .foregroundColor(.orange)
-            Text("Hi CS193p!")
-                .font(.title)
-        }
-        
-//            RoundedRectangle( cornerRadius: 12)
-//                .strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10.2]))
-//                .foregroundColor(.orange)
-//                .imageScale(.small)
-//                .padding()
-        
+    var body: some View {
         HStack {
             CardView(isFaceUp: true)
             CardView()
@@ -34,23 +16,6 @@ struct ContentView: View {
             CardView()
         }
         .foregroundColor(.orange)
-        .padding()
-        
-        VStack {
-            Text(message)
-                .font(.title)
-            
-            Button (action: {
-                isToggled.toggle()
-                message = isToggled ? "Button tapped!" : "It's working!"
-            }) {
-                Text("Tap me")
-                    .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-            }
-        }
         .padding()
     }
 }
@@ -63,24 +28,11 @@ struct CardView: View {
             if isFaceUp {
                 RoundedRectangle( cornerRadius: 12)
                     .foregroundColor(.white)
-                
                 RoundedRectangle( cornerRadius: 12)
-                    .strokeBorder(lineWidth: 5)
-                    .foregroundColor(.yellow)
-                
-                Rectangle()
-                                .fill(Color.red)
-                                .frame(width: 45, height: 45)
-                                .padding()
-                
-                Circle()
-                    .fill(Color.black)
-                    .frame(width: 50, height: 50)
-                
+                    .strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
                 RoundedRectangle( cornerRadius: 12)
-//                    .foregroundColor(.orange)
             }
         })
     }
