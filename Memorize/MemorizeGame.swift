94f29f5fba7798bf9fdd51022ec4077dd77a8182
kibodo <kibodo@ya.ru>
2025-06-16 19:42:20 +0200

    feat: add  MemorizeGame

---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,23 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by N L on 16. 6. 2025..
+//
+
+import Foundation
+
+struct MemorizeGame {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: String
+        
+    }
+}
