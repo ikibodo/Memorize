3afa10d3eabe4aa808afdb658ee57222d219eba6
kibodo <kibodo@ya.ru>
2025-06-23 17:58:55 +0200

    style: explaining view with background

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,16 +15,20 @@ struct EmojiMemoryGameView: View {
             ScrollView {
                 cards
                     .animation(.default, value: viewModel.cards)
+                    .background(.red) // только скрол
             }
             Button("Shuffle") {
                 viewModel.shuffle()
             }
+            .background(.green)
         }
+            .background(.yellow) // только вью
             .padding()
+            //.background(.yellow) // весь экран
         }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.fixed(40))], spacing: 0) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65), spacing: 0)], spacing: 0) {
             ForEach(viewModel.cards) { card in
                 CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
