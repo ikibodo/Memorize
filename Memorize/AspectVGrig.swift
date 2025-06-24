63a1b2222b2a9f6a199be3d92d03e004c608ea2f
kibodo <kibodo@ya.ru>
2025-06-24 13:42:58 +0200

    refactor: add content to AspectVGrig

--- Memorize/AspectVGrig.swift
+++ Memorize/AspectVGrig.swift
@@ -7,16 +7,46 @@
 
 import SwiftUI
 
-struct AspectVGrig: Layout {
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        <#code#>
+struct AspectVGrig<Item: Identifiable, ItemView: View>: View {
+// struct AspectVGrig<Item>: View where Item: Identifiable { // другой вариант написания того что выше
+    var items: [Item]
+    var aspectRatio: CGFloat = 1
+    var content: (Item) -> ItemView // функция(замыкание), которая принимает элементы и возвращает для него View
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWithThatFits(
+                count: items.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio
+            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(items) { item in
+                    content(item)
+                        .aspectRatio(aspectRatio, contentMode: .fit)
+                }
+            }
+        }
     }
     
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        <#code#>
+    func gridItemWithThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0 // вместо Int
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)            }
+            columnCount += 1
+        } while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
-    
-
 }
 
 //#Preview {
