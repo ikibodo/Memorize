29d295db26cdb837d71c5fab14ecdefd9cc71dda
kibodo <kibodo@ya.ru>
2025-09-15 11:29:47 +0200

    fix: emojis don't flicker

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -15,8 +15,10 @@ struct MemoryGame <CardContent> where CardContent: Equatable { // Model
         cards = []
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
-            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
+//            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+//            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
+            cards.append(Card(content: content, id: UUID().uuidString))
+            cards.append(Card(content: content, id: UUID().uuidString))
         }
     }
     
